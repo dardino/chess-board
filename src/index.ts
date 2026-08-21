30d1e579c7f74ce0bc2f3eb493b5631246ca9585
Gabriele Brunori <gabriele.brunori@grupposandonato.it>
2026-08-21 15:36:35 +0200

    feat: enhance FEN/FFEN support with auto-detection and unified API in documentation and components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { ChessBoard, type CellClickPiece, type CellClickEventDetail, type PieceInfo } from './ChessBoard';
-export { ChessPiece, type ChessPieceType, type ChessPieceColor, type ChessPieceRotation } from './ChessPiece';
+export { ChessBoard, type CellClickEventDetail, type CellDecorator, type FenChangeEventDetail, type PieceInfo, type PieceInfoWithSquare, type Square } from './ChessBoard';
+export { ChessPiece, type ChessPieceColor, type ChessPieceRotation, type ChessPieceType } from './ChessPiece';
 export * from './fen';
