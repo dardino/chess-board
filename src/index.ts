86477efc507465c5913d4c17b904bc1a1eac2441
Gabriele <dardino@users.noreply.github.com>
2026-08-24 22:48:42 +0200

    fix build

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { ChessBoard, type CellClickEventDetail, type CellDecorator, type FenChangeEventDetail, type PieceInfo, type PieceInfoWithSquare, type Square } from './ChessBoard';
-export { ChessPiece, type ChessPieceColor, type ChessPieceRotation, type ChessPieceType } from './ChessPiece';
+export { ChessPiece } from './ChessPiece';
 export * from './fen';
